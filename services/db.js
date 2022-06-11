4f1117c53eb33425166d5d63fcc36de49f9fe0bf
Arslan <alijansoomro0@gmail.com>
2022-06-11 20:09:51 +0500

    Connect to database

--- services/db.js
+++ services/db.js
@@ -1,2 +1,14 @@
 // Connects to Database and exports the database connection
 // Code To create schemas must also be added in this folder with corresponding schema names.
+
+import mysql from "mysql2";
+
+//Create a pooling connection
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'funk'
+});
+
+export default pool.promise();
