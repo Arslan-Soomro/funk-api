704c06e3e515b66c8da0bbea33b164325a8afed3
Arslan <alijansoomro0@gmail.com>
2022-06-14 09:58:20 +0500

    Add image attribute into buyer schema

--- services/schemas.js
+++ services/schemas.js
@@ -8,7 +8,8 @@ const BuyerSchema = `buyer(
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL, 
     email VARCHAR(60) NOT NULL, 
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    image VARCHAR(255)
 )`;
 
 const StatusSchema = `status(
