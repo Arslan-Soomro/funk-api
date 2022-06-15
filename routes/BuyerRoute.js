ee267811efe0f00bd97eef57ba43856ddb577719
Arslan <alijansoomro0@gmail.com>
2022-06-15 15:20:11 +0500

    Add Buyer Signup Route

--- routes/BuyerRoute.js
+++ routes/BuyerRoute.js
@@ -3,34 +3,49 @@ const router = express.Router();
 const { checkSchema, validationResult } = require("express-validator");
 const db = require("../services/db");
 const { BuyerValidationSchema } = require("../services/validationSchemas");
+const { hashEncrypt } = require("../utils/authUtils");
 const { RespondToValidationErrors } = require("../utils/utils");
 
 router.get("/", (req, res) => res.send("You Have Reached Buyer Route."));
 
-//Adds a user into database and responds with a token
-//encrypt the password and then store
-//Generate a token
 router.post(
   "/signup",
   checkSchema(BuyerValidationSchema()),
   RespondToValidationErrors,
   async (req, res) => {
-    const { username, email, first_name, last_name, password } = req.body;
-    const query = `
+    try {
+      //Extract required fields form req.body
+      let { username, email, first_name, last_name, password } = req.body;
+
+      //Encrypt Password
+      password = await hashEncrypt(password);
+
+      //query for adding a buyer
+      const query = `
         INSERT INTO buyer
         (username, email, first_name, last_name, password)
         VALUES (?, ?, ?, ?, ?)
     `;
-    const result = await db.execute(query, [
-      username,
-      email,
-      first_name,
-      last_name,
-      password,
-    ]);
-    console.log(result);
-    res.send("Signup");
+
+      //add the buyer
+      await db.execute(query, [
+        username,
+        email,
+        first_name,
+        last_name,
+        password,
+      ]);
+
+      res.json("Buyer Signedup Successfully !");
+    } catch (err) {
+      console.log("Error@BuyerSignup: " + err.message);
+      res.status(500).json({ error: "Error occured at server" });
+    }
   }
 );
+
+router.post('/signin', (req, res) => {
+
+});
 
 module.exports = router;
