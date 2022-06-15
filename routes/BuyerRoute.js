0660a750f6d974f3b07d30361adabbbed7dcb83b
Arslan <alijansoomro0@gmail.com>
2022-06-15 14:10:02 +0500

    Create Validation Schema for Buyer Signup

--- routes/BuyerRoute.js
+++ routes/BuyerRoute.js
@@ -1,27 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { checkSchema, validationResult } = require('express-validator');
+const { BuyerValidationSchema } = require('../services/validator');
 
 router.get('/', (req, res) => res.send("You Have Reached Buyer Route."));
 
-router.post('/signup', checkSchema({
-    username : {
-        in: ["body"],
-        errorMessage: "Username missing !",
-        isString: true,
-        toLowerCase: true,
-        trim: true,
-        isLength: { options: {min: 6}, errorMessage: "username must be 6 characters long" },
-    }
-}) ,(req, res) => {
-    console.log(req.body);
+router.post('/signup', checkSchema(BuyerValidationSchema()) ,(req, res) => {
     const errors = validationResult(req);
     console.log(errors)
-    if(errors.isEmpty()){
-        console.log("No Errors");
-    }else {
-        console.log("Errors");
-    }
     res.send("Signing UP");
 })
 
---
+++ services/validator.js
@@ -0,0 +1,64 @@
+const BuyerValidationSchema = () => {
+  return {
+    username: {
+      in: ["body"],
+      errorMessage: "Username is required !",
+      isString: true,
+      toLowerCase: true,
+      trim: true,
+      isLength: {
+        options: { min: 3, max: 30 },
+        errorMessage: "Username must be longer than 3 characters and smaller than 30 characters",
+      },
+    },
+    email : {
+      in: ["body"],
+      exists: {
+        errorMessage: "Email is required !",
+        bail: true,
+      }, 
+      isEmail: {
+        errorMessage: "Invalid Email Address !",
+        bail: true
+      },
+      toLowerCase: true,
+      trim: true
+    },
+    first_name: {
+      in: ["body"],
+      errorMessage: "First name is required !",
+      isString: true,
+      trim: true,
+      toLowerCase: true,
+      isLength: {
+        options: { min: 3, max: 30 },
+        errorMessage: "First name must be longer than 3 characters and smaller than 30 characters",
+      },
+    },
+    last_name: {
+      in: ["body"],
+      errorMessage: "Last name is required !",
+      isString: true,
+      trim: true,
+      toLowerCase: true,
+      isLength: {
+        options: { min: 3, max: 30 },
+        errorMessage: "Last name must be longer than 3 characters and smaller than 30 characters",
+      },
+    },
+    password: {
+      in: ["body"],
+      errorMessage: "Last name is required !",
+      isString: true,
+      trim: true,
+      isLength: {
+        options: { min: 4, max: 16},
+        errorMessage: "Password must be longer than 3 characters and smaller than 30 characters",
+      }
+    }
+  };
+};
+
+module.exports = {
+    BuyerValidationSchema
+}
