36379ca3d7ca58d25d956ca2b0e819433ecaee5f
Arslan <alijansoomro0@gmail.com>
2022-06-15 14:44:47 +0500

    Add Complete Validation To Signup Buyer

--- routes/BuyerRoute.js
+++ routes/BuyerRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { checkSchema, validationResult } = require('express-validator');
-const { BuyerValidationSchema } = require('../services/validator');
+const { BuyerValidationSchema } = require('../services/validationSchemas');
+const { RespondToValidationErrors } = require('../utils/utils');
 
 router.get('/', (req, res) => res.send("You Have Reached Buyer Route."));
 
-router.post('/signup', checkSchema(BuyerValidationSchema()) ,(req, res) => {
+router.post('/signup', checkSchema(BuyerValidationSchema()), RespondToValidationErrors, (req, res) => {
     const errors = validationResult(req);
-    console.log(errors)
+    //console.log(errors);
     res.send("Signing UP");
 })
 
--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,20 @@
 //Utitlity functions that aid in performing something should be defined in here.
+
+const { validationResult } = require("express-validator");
+
+const RespondToValidationErrors = (req, res, next) => {
+    let errors = validationResult(req);
+
+    if(errors.isEmpty()){
+        next();
+    }else {
+        res.json({
+            error: errors.errors[0].msg
+        });
+    }
+
+};
+
+module.exports = {
+    RespondToValidationErrors
+}
